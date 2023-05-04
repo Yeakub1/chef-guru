89b402b39c10db803731ccf4391b41c717c38cdc
Yeakub1 <mryeakub10@gmail.com>
2023-05-04 21:48:36 +0600

    blog section

--- src/components/Home/Blog/Blog.jsx
+++ src/components/Home/Blog/Blog.jsx
@@ -1,114 +1,66 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
-import blog1 from '../../../assets/blog1.jpg'
-import blog2 from '../../../assets/blog2.jpg'
-import blog3 from '../../../assets/blog3.jpg'
-import blog4 from '../../../assets/blog4.jpg'
-import blog5 from '../../../assets/blog5.jpg'
-import blog6 from '../../../assets/blog6.jpg'
+import { Container } from 'react-bootstrap';
 
 const Blog = () => {
-    return (
-      <Container className=" ">
-        <h2 className="text-center mb-5 mt-5">Blog</h2>
-        <div className=" row justify-content-between">
-          <div className="col-md-6 mb-5">
-            <Card>
-              <Card.Img variant="top" src={blog1} />
-              <Card.Body className="text-justify">
-                <p>
-                  It is a long established fact that a reader will be distracted
-                  by the readable content of a page when looking at its layout.
-                  The point of using Lorem Ipsum is that it has a more-or-less
-                  normal distribution of letters, as opposed to using ‘Content
-                  here, content here’, making it look like readable English.
-                  Many desktop publishing packages and web page editors now use.
-                </p>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-6">
-            <Card>
-              <Card.Img variant="top" src={blog2} />
-              <Card.Body className="text-justify">
-                <p>
-                  Cooking Dining Experience It is a long established fact that a
-                  reader will be distracted by the readable content of a page
-                  when looking at its layout. The point of using Lorem Ipsum is
-                  that it has a more-or-less normal distribution of letters, as
-                  opposed to using
-                </p>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-6">
-            <Card>
-              <Card.Img variant="top" src={blog3} />
-              <Card.Body className="text-justify">
-                <p>
-                  The History of Kitchens and Cooks gives further intimation on
-                  Mr Boulanger usual menu, stating confidently that "Boulanger
-                  served salted poultry and fresh eggs, all presented without a
-                  tablecloth on small marble tables". Numerous commentators have
-                  also referred to the supposed restaurant owner's eccentric
-                  habit of touting for custom outside his establishment, dressed
-                  in aristocratic fashion and brandishing a sword
-                </p>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-6 mb-5">
-            <Card>
-              <Card.Img variant="top" src={blog4} />
-              <Card.Body className="text-justify">
-                <p>
-                  Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit
-                  amet magna suscipit, fermentum mattis erat rutrum. Sed
-                  suscipit libero lectus, at ullamcorper erat feugiat eu. Nam
-                  posuere ultrices nibh ut sagittis. Etiam arcu turpis,
-                  elementum ac nulla vel, tristique cursus libero. Fusce
-                  feugiat, justo at mattis tincidunt, velit ante congue ante, et
-                  lacinia metus ipsum a risus. Vivamus hendrerit porta libero
-                  nec imperdiet. Praesent interdum nisl nec facilisis dapibus.
-                </p>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-6">
-            <Card>
-              <Card.Img variant="top" src={blog5} />
-              <Card.Body className="text-justify">
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam, eaque
-                  ipsa quae ab illo inventore veritatis et quasi architecto
-                  beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-                  quia voluptas sit aspernatur. adipisci velit, sed quia non
-                  numquam eius modi tempora incidunt ut labore et dolore magnam
-                  aliquam quaerat voluptatem.
-                </p>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-6">
-            <Card>
-              <Card.Img variant="top" src={blog6} />
-              <Card.Body className="text-justify">
-                <p>
-                  Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-                  amet, consectetur, adipisci velit, sed quia non numquam eius
-                  modi tempora incidunt ut labore et dolore magnam aliquam
-                  quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-                  exercitationem ullam corporis suscipit laboriosam, nisi ut
-                  aliquid ex ea commodi consequatur? Quis autem vel eum iure
-                  reprehenderit.
-                </p>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      </Container>
-    );
+  return (
+    <Container>
+      <div className="">
+        <h4>
+          <span className="text-primary">
+            1. uncontrolled and controlled components:
+          </span>{" "}
+          In React, controlled components refer to components that have their
+          state and behavior controlled by the parent component. These
+          components rely on props passed down from the parent component to
+          update their state and behavior. Uncontrolled components refer to
+          components that manage their own state internally.
+        </h4>
+      </div>
+      <div className="mt-5">
+        <h4>
+          <span className="text-primary">
+            2. validate React props using PropTypes:
+          </span>{" "}
+          Props are an important mechanism for passing the read-only attributes
+          to React components. The props are usually required to use correctly
+          in the component. If it is not used correctly, the components may not
+          behave as expected. Hence, it is required to use props validation in
+          improving react components.
+        </h4>
+      </div>
+
+      <div className="mt-5">
+        <h4>
+          <span className="text-primary">3. node.js :</span> NodeJS is the
+          package, which provides the JavaScript run-time environment, whereas
+          Express is a framework that sits on top of NodeJS and helps us to
+          handle requests and responses.
+        </h4>
+        <h4>
+          <span className="text-primary">3. Express.js: :</span> Express is a
+          small framework that sits on top of Node.jss web server functionality
+          to simplify its APIs and add helpful new features. It makes it easier
+          to organize your applications functionality with middle ware and
+          routing. It adds helpful utilities to Node.jss HTTP objects. It
+          facilitates the rendering of dynamic HTTP objects.
+        </h4>
+      </div>
+
+      <div className="mt-5">
+        <h4>
+          <span className="text-primary">
+            3. custom hook and create a custom hook :
+          </span>{" "}
+          A custom Hook is a JavaScript function whose name starts with use
+          and that may call other Hooks.
+          <br />
+          Custom React JS hooks offer reusability as when a custom hook is
+          created, it can be reused easily, which makes the code cleaner and
+          reduces the time to write the code.
+        </h4>
+      </div>
+    </Container>
+  );
 };
 
 export default Blog;
